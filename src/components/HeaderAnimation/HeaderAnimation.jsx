3302ff25002e1c1f26c4de59eaf952255c09fb90
Carlos Diego Simon Hurtado <diegoestushoi@gmail.com>
2022-12-22 21:08:19 -0300

    Agregado animacion con GSAP a HeaderAnimation

--- src/components/HeaderAnimation/HeaderAnimation.jsx
+++ src/components/HeaderAnimation/HeaderAnimation.jsx
@@ -1,6 +1,63 @@
 import { HeaderAnimationContainer } from "../StyledComponents/Header/HeaderAnimation.styled";
+import { useLayoutEffect } from "react";
+import gsap from "gsap";
 
 const HeaderAnimation = () => {
+
+    useLayoutEffect( () => {
+
+        const ctx = gsap.context( () => {
+
+            let timeLineApear = gsap.timeline();
+
+            timeLineApear.to(".a1", {
+                y: 0,
+                opacity: 1,
+                delay: 2.5,
+                duration: 0.38
+            }).to(".b1", {
+                y: 0,
+                opacity: 1,
+                duration: 0.38
+            }).to(".c2", {
+                y: 0,
+                opacity: 1,
+                duration: 0.38
+            }).to(".a2", {
+                y: 0,
+                opacity: 1,
+                duration: 0.38
+            }).to(".b2", {
+                y: 0,
+                opacity: 1,
+                duration: 0.38
+            }).to(".c3", {
+                y: 0,
+                opacity: 1,
+                duration: 0.38
+            }).to(".a3", {
+                y: 0,
+                opacity: 1,
+                duration: 0.38
+            }).to(".b3", {
+                y: 0,
+                opacity: 1,
+                duration: 0.38
+            }).to(".c4", {
+                y: 0,
+                opacity: 1,
+                duration: 0.38
+            }).to(".b4", {
+                y: 0,
+                opacity: 1,
+                duration: 0.38
+            });
+
+        })
+        return () => ctx.revert();
+
+    }, []);
+
     return(
         <HeaderAnimationContainer>
                 <div class="a1"></div>
--- src/components/StyledComponents/Header/HeaderAnimation.styled.jsx
+++ src/components/StyledComponents/Header/HeaderAnimation.styled.jsx
@@ -1,39 +1,69 @@
 import styled from "styled-components";
 
 export const HeaderAnimationContainer = styled.section`
+    
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 15%;
+    height: calc(100% - 25px);
+    div{
+        transform: translateY(-30%);
+        opacity: 0;
+        border: 3px solid black;
+        transition: all 1s ease;
+        &:hover{
+            transform: scale(1.06);
+        }
+    }
+    display: grid;
+    grid:
+        "b1 c1" 1fr
+        "b1 c2" 1fr
+        "b2 c2" 1fr
+        "b2 c2" 1fr
+        "b2 c3" 1fr
+        "b3 c3" 1fr
+        "b3 c3" 1fr
+        "b3 c4" 1fr
+        "b4 c4" 1fr
+        / 1fr 1fr;
+    gap: 5px;
+
+    .a1 { display: none; }
+    .a2 { display: none; }
+    .a3 { display: none; }
+    .b1 { grid-area: b1; background-color: #232b69;}
+    .b2 { grid-area: b2; background-color: #de2d27;}
+    .b3 { grid-area: b3; background-color: #ffc90a;}
+    .b4 { grid-area: b4; background-color: #232b69;}
+    .c1 { grid-area: c1; background-color: #232b69;}
+    .c2 { grid-area: c2; background-color: #232b69;}
+    .c3 { grid-area: c3; background-color: #de2d27;}
+    .c4 { grid-area: c4; background-color: #ffc90a;}
+
+    @media(min-width: 768px){
+        grid:
+        "a1 b1 c1" 1fr
+        "a1 b1 c2" 1fr
+        "a1 b2 c2" 1fr
+        "a2 b2 c2" 1fr
+        "a2 b2 c3" 1fr
+        "a2 b3 c3" 1fr
+        "a3 b3 c3" 1fr
+        "a3 b3 c4" 1fr
+        "a3 b4 c4" 1fr
+        / 1fr 1fr 1fr;
+
+        .a1 { display: block; grid-area: a1; background-color: #de2d27;}
+        .a2 { display: block; grid-area: a2; background-color: #ffc90a;}
+        .a3 { display: block; grid-area: a3; background-color: #232b69;}
+    }
     @media(min-width: 1024px){
         position: absolute;
         top: 0;
-        right: 10%;
+        right: 5%;
         width: 20%;
-        height: calc(100% - 25px);
-        div{
-            border: 4px solid black;
-        }
-        display: grid;
-        grid:
-            "a1 b1 c1" 1fr
-            "a1 b1 c2" 1fr
-            "a1 b2 c2" 1fr
-            "a2 b2 c2" 1fr
-            "a2 b2 c3" 1fr
-            "a2 b3 c3" 1fr
-            "a3 b3 c3" 1fr
-            "a3 b3 c4" 1fr
-            "a3 b4 c4" 1fr
-            / 1fr 1fr 1fr;
         gap: 8px;
-
-        .a1 { grid-area: a1; background-color: #de2d27;}
-        .b1 { grid-area: b1; background-color: #232b69;}
-        .c1 { grid-area: c1; background-color: #232b69;}
-        .c2 { grid-area: c2; background-color: #232b69;}
-        .b2 { grid-area: b2; background-color: #de2d27;}
-        .a2 { grid-area: a2; background-color: #ffc90a;}
-        .c3 { grid-area: c3; background-color: #de2d27;}
-        .b3 { grid-area: b3; background-color: #ffc90a;}
-        .a3 { grid-area: a3; background-color: #232b69;}
-        .c4 { grid-area: c4; background-color: #ffc90a;}
-        .b4 { grid-area: b4; background-color: #232b69;}
-        }
-`;
+    }
+    `;
